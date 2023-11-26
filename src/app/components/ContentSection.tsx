aa2ce64b32b2b6f9c00f2a2164c0ab204a832da5
apv-21 <agrawalapoorv021@gmail.com>
2023-11-26 14:49:17 +0530

    Added resolution to image in Post

--- src/app/components/ContentSection.tsx
+++ src/app/components/ContentSection.tsx
@@ -5,16 +5,24 @@ import Post from "./Post";
 
 const ContentSection = ({ content }: { content: any }) => {
   return (
-    <Flex gap="24px" flexDirection="column" position="absolute" top="80px">
+    <Flex
+      gap="24px"
+      flexDirection="column"
+      position="absolute"
+      top="80px"
+      width="100%"
+    >
       {content?.edges?.map((card, index: number) => {
         console.log("test card", card);
         const {
           name = "",
           experts = [{ firstName: "", lastName: "", company: "" }],
+          image = { uri: "" },
         } = card || {};
         return (
           <Post
             key={index}
+            imageUrl={image.uri}
             podcastName={name}
             hostFirstName={experts[0].firstName}
             hostLastName={experts[0].lastName}
--- src/app/components/Post.tsx
+++ src/app/components/Post.tsx
@@ -3,6 +3,7 @@ import { Image } from "@chakra-ui/react";
 import { Text, Flex } from "@chakra-ui/react";
 
 interface PostProps {
+  imageUrl: string;
   podcastName: string;
   hostFirstName: string;
   hostLastName: string;
@@ -10,19 +11,24 @@ interface PostProps {
 }
 
 const Post = ({
+  imageUrl,
   podcastName,
   hostFirstName,
   hostLastName,
   companyName,
 }: PostProps) => {
+  const imageArray = imageUrl?.split("tigerhall.io");
+  const resizedImage = `${imageArray[0]}tigerhall.io/resize/250x120${imageArray[1]}`;
+
   return (
-    <Flex flexDirection="column" width="80%" margin="0 auto">
+    <Flex flexDirection="column" width="68%" margin="0 auto">
       <Image
-        src="https://willnorris.com/api/imageproxy/200x/https://willnorris.com/images/imageproxy/small-things.jpg"
+        src={resizedImage}
         alt="Image"
         borderRadius="8px 8px 0 0"
         width="100%"
         height="100%"
+        backgroundSize="cover"
       />
       <Flex
         justifyContent="space-between"

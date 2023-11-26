47f751e1f1056d7e255d519f04b8f4055aec3b33
apv-21 <agrawalapoorv021@gmail.com>
2023-11-26 13:05:09 +0530

    Added Search UI

--- src/app/components/Search.tsx
+++ src/app/components/Search.tsx
@@ -1,5 +1,67 @@
-const Search = () => {
-  return <div>Search bar</div>;
+"use client";
+
+import { Flex } from "@chakra-ui/react";
+import Image from "next/image";
+import { useRef } from "react";
+import { Input } from "@chakra-ui/react";
+
+export const Search = () => {
+  const inputRef = useRef(null);
+
+  const handleOnChange = async (event: { target: { value: string } }) => {
+    console.log("Query:", event.target.value);
+  };
+
+  const handleOnKeyUp = (event: { key: string }) => {
+    if (event.key === "Enter") {
+      const query = inputRef.current.value;
+      console.log("Search query:", query);
+    }
+  };
+
+  const handleOnKeyDown = (event: { key: string }) => {
+    if (event.key === "Escape") {
+      inputRef.current.value = "";
+    }
+  };
+
+  return (
+    <Flex
+      width="67%"
+      border="1px solid #797670"
+      background="linear-gradient(0deg, #797670, #797670),
+  linear-gradient(0deg, #383733, #383733)"
+      borderRadius="4px"
+      margin="0 auto"
+      padding="12px 10px"
+      position="sticky"
+    >
+      <Flex flexDirection="row" height="16px">
+        <Image
+          src={`/SearchIcon.png`}
+          alt="search-icon"
+          width={16}
+          height={16}
+        ></Image>
+        <Input
+          ref={inputRef}
+          placeholder="Search"
+          onChange={handleOnChange}
+          onKeyUp={handleOnKeyUp}
+          onKeyDown={handleOnKeyDown}
+          bg="transparent"
+          color="white"
+          border="none"
+          _focus={{
+            outline: "none",
+            boxShadow: "none",
+            borderColor: "transparent",
+          }}
+          ml="8px"
+        />
+      </Flex>
+    </Flex>
+  );
 };
 
 export default Search;
